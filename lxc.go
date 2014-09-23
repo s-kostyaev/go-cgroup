f94424674c1a5b46c0e0634c582363da66dfe835
Kostyaev Sergey <s.kostyaev@office.ngs.ru>
2014-09-23 15:12:42 +0000

    inital commit

---
+++ lxc.go
@@ -0,0 +1,40 @@
+package lxc
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+type Container struct {
+	Name  string
+	State string
+	IP    string
+}
+
+func GetContainers() (containers []Container) {
+	cmd := exec.Command("heaver", "-L")
+	cmd.Stdin = strings.NewReader("")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rawcontainers := strings.Split(strings.Trim(out.String(), "\n"), "\n")
+	for _, rawcontainer := range rawcontainers {
+		var con Container
+		cont_str := strings.Split(strings.Trim(rawcontainer, " "), " ")
+		con.Name = strings.Trim(cont_str[0], ":")
+		con.State = strings.Trim(cont_str[1], ",")
+		con.IP = strings.Split(cont_str[3], "/")[0]
+		containers = append(containers, con)
+	}
+	return
+}
+
+func (container Container) String() string {
+	return fmt.Sprintf("{Name: %s state: %s, ip: %s}", container.Name, container.State, container.IP)
+}
