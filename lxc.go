ec3ccefa0b88e4230db473dded6090a44044ce7d
Kostyaev Sergey <s.kostyaev@office.ngs.ru>
2014-10-03 11:40:49 +0000

    move lxc/memory/monitor utils to lxc package

--- lxc.go
+++ lxc.go
@@ -2,9 +2,20 @@ package lxc
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"strings"
+)
+
+const (
+	memoryPathPrefix    = "/sys/fs/cgroup/memory/lxc"
+	memoryLimitFilename = "memory.limit_in_bytes"
+	memoryUsageFilename = "memory.usage_in_bytes"
 )
 
 type Container struct {
@@ -36,4 +47,44 @@ func GetContainers() ([]Container, error) {
 
 func (container Container) String() string {
 	return fmt.Sprintf("%+v", container)
+}
+
+func GetParam(cgroup, containerName, paramName string) (string, error) {
+	switch cgroup {
+	case "memory":
+		return memoryGetParam(containerName, paramName)
+	default:
+		return "", errors.New(fmt.Sprintf("Undefined cgroup %s",
+			cgroup))
+	}
+}
+
+func GetParamInt(cgroup, containerName, paramName string) (int, error) {
+	res, err := GetParam(cgroup, containerName, paramName)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.Atoi(res)
+}
+
+func memoryGetParam(containerName, paramName string) (string, error) {
+	file := ""
+	switch paramName {
+	case "limit":
+		file = memoryLimitFilename
+	case "usage":
+		file = memoryUsageFilename
+	default:
+		return "", errors.New("Undefined parameter name")
+	}
+	paramPath := filepath.Join(memoryPathPrefix, containerName, file)
+	if _, err := os.Stat(paramPath); err != nil {
+		paramPath = filepath.Join(memoryPathPrefix,
+			fmt.Sprintf("%s-1", containerName), file)
+	}
+	content, err := ioutil.ReadFile(paramPath)
+	if err != nil {
+		return "", err
+	}
+	return strings.Trim(string(content), "\n"), nil
 }
